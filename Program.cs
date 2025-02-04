b82d8c17596f1017a933c2511658df1fbfa271f9
Marin Vyse <362587859@GoTVDSB.ca>
2025-02-04 14:45:02 -0500

    Finished

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Topic_1_Assignment
+﻿using System.Runtime.CompilerServices;
+
+namespace Topic_1_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { // Marin Vyse
+            Console.WriteLine("Hello, My name is MARIN");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used to for notes and to show thinking");
+
+            Console.WriteLine("M   M V   V");
+            Console.WriteLine("MM MM V   V");
+            Console.WriteLine("MM MM V   V");
+            Console.WriteLine("M M   V   V");
+            Console.WriteLine("M   M V   V");
+            Console.WriteLine("M   M  V V");
+            Console.WriteLine("M   M   V");
+
+          
+            Console.WriteLine("This was made by using vision studio");
+
         }
-    }
-}
+
